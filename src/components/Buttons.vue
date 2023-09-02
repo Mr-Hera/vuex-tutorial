d20c07577126d96c07988cf7c00d73c86c4c1bbe
Mr-Hera <randellhera@gmail.com>
2023-09-02 03:16:13 +0300

    Feat: vuex updated & completed

---
+++ src/components/Buttons.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="buttons">
+      <button
+        @click.prevent="$store.dispatch('decreaseCounter')"        
+      >-</button>
+      <button
+        @click.prevent="$store.dispatch('increaseCounter')"        
+      >+</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,49 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: "red"
   },
   getters: {
+    counterSquared(state) {
+      return state.counter * state.counter;
+    }
   },
   mutations: {
+    increaseCounter(state, randNumber) {
+      console.log('🚀 ~ random number: ', randNumber)
+      state.counter += randNumber;
+    },
+    decreaseCounter(state, randNumber) {
+      console.log('🚀 ~ random number: ', randNumber)
+      state.counter -= randNumber;
+    },
+    setColorCode(state, newValue) {
+      console.log('🚀 ~ New color: ', newValue)
+      state.colorCode = newValue;
+    }
   },
   actions: {
+    increaseCounter({ commit }) {
+      console.log('🚀 ~ increaseCounter(action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+        .then(res => {
+          commit('increaseCounter', res.data)
+        })
+    },
+    decreaseCounter({ commit }) {
+      console.log('🚀 ~ increaseCounter(action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+        .then(res => {
+          commit('decreaseCounter', res.data)
+        })
+    },
+    setColorCode({ commit }, newValue) {
+      console.log('🚀 ~ increaseCounter(action)')
+      commit('setColorCode', newValue)
+    },
   },
   modules: {
   }
